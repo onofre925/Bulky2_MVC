b401ae9f9737540bfb448669335091e5ec960b9d
Emanuel Onofre <emanuelonofre@csus.edu>
2023-06-13 18:02:27 -0700

    Retrieving implementation from Program.cs to the CategoryController.cs which is a step before

--- BulkyWeb2/Controllers/CategoryController.cs
+++ BulkyWeb2/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb2.Data;
+using BulkyWeb2.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb2.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
